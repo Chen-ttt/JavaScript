2d1fdf120d429e0630317fb9394157ed68be7d52
Tong Chen <1141425427@qq.com>
2022-07-03 15:41:03 +0100

    Tips of Convert Data Type

--- script/function.js
+++ script/function.js
@@ -27,11 +27,17 @@ var functionTest2 = function(){
  * 形参和实参的数量可以不相等,不会报错
  * 形参: 通过函数名.length查看形参个数
  * 实参: 函数内可通过arguments查看传入的实参,arguments实质上是一个数组
+ * Tips: 1. 可以利用arguments和for循环做实参的累加,累乘
+ *       2. 尝试给为传入实参的形参赋值或打印,不会报错,会显示undefined
+ *       3. 形参储存在stack内存中,实参数组arguments储存在heap内存中.两者之间存在映射关系，因此当给形参a赋值时，arguments[0]的值也会相应改变;而给c赋值时,若arguments[2]没有相应的实参传入,则赋值无法完成。
  */
 function test3(a, b, c){ // a,b,c - 形参
     console.log("实参个数 ", arguments.length);
     console.log("形参个数 ", test3.length);
     console.log(a, b, c);
+    c = 9;
+    console.log(arguments[0]);
 }
-test3(1,2); // 1,2 - 实参 // 不会报错, 1 2 undefined
+test3(1,2); // 1,2 - 实参
 test3(1,2,3,4);
+
