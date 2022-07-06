20f67f29646339b0dad8231c29ea01042b801e4f
Tong Chen <1141425427@qq.com>
2022-07-06 17:19:23 +0100

    Basic Operation API of Array

--- script/referenceData.js
+++ script/referenceData.js
@@ -2,7 +2,27 @@
 
 // array
 var arr = [1, 2, undefined, null];
-console.log(arr.length);
+console.log("arr length:", arr.length);
+
+// 增
+arr.push(4);
+console.log("After add:", arr);
+arr[arr.length] = 5;
+console.log("After add:", arr);
+// 删
+// splice函数可以删除数组中某些元素,第一个参数表示要删除的元素下标,第二个参数表示要删除的元素的个数
+arr.splice(2,1); // 删除arr[2]
+console.log("After delete:", arr);
+arr.splice(arr.indexOf(null),1); // 删除arr其中值为null的元素
+console.log("After delete:", arr);
+arr.splice(1,2); // 删除arr[1]和[2]两个元素
+console.log("After delete:", arr);
+// 改
+arr[0] = 99;
+console.log("After change:", arr);
+// 查
+// indexOf函数可以查找元素在数组中的下标,查找失败则返回-1
+console.log("View index of \"99\":", arr.indexOf('99'));
 
 // object: key-value pair
 var person = {
