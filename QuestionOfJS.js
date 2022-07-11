da8b8047e5b180fb1fb9786ad44f9b94257d7fc0
Tong Chen <1141425427@qq.com>
2022-07-11 23:59:19 +0100

    Questions of Interviews

--- QuestionOfJS.js
+++ QuestionOfJS.js
@@ -1,5 +1,6 @@
 // JS笔试题
 
+// 1.
 var a = {},
     b = {key: 'b'},
     c = {key: 'c'};
@@ -7,3 +8,15 @@ var a = {},
 a[b] = 123;
 a[c] = 456;
 console.log("第一题", a[b]);
+
+// 2.
+var name = "world";
+;(function (){
+    // console.log(name);
+    if(typeof name === 'undefined'){
+        var name = 'J';
+        console.log(name);
+    } else {
+        console.log("hello");
+    }
+})();
