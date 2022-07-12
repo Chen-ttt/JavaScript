56d9271bb5f94023363703dda04d9cfbe9b06bc6
Tong Chen <1141425427@qq.com>
2022-07-12 01:01:08 +0100

    Call & Apply & Bind

--- script/callApply.js
+++ script/callApply.js
@@ -12,4 +12,22 @@ function add(c,d){
 
 var s = {a:1, b:2};
 console.log(add.call(s,3,4)); // 1+2+3+4 = 10
-console.log(add.apply(s,[5,6])); // 1+2+5+6 = 14 
+console.log(add.apply(s,[5,6])); // 1+2+5+6 = 14
+
+// 解决原型链问题 - 继承父类属性
+Teacher.prototype = {
+    job: 'Math'
+};
+function Teacher(name, mSkill){
+    this.name = name;
+    this.mSkill = mSkill;
+}
+
+function Student(name, mSkill, age, major){
+    Teacher.apply(this, [name, mSkill]);
+    this.age = age;
+    this.major = major;
+}
+
+var student = new Student('Tong', 'JS', 18, 'CS');
+console.log(student);
