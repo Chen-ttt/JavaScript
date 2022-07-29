a704c375ac741b6b2940ef6d589160fdf4768a60
Tong Chen <1141425427@qq.com>
2022-07-30 00:00:52 +0100

    Example 3 of Event loop

--- testAsync/test3.js
+++ testAsync/test3.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-07-27 14:59:50
- * @LastEditTime: 2022-07-27 15:34:34
+ * @LastEditTime: 2022-07-29 16:14:58
  * @LastEditors:  
  */
 async function async1 () {
@@ -19,13 +19,22 @@ setTimeout(function () {
 }, 0)
 
 async function async3 () {
-  return new Promise(function (resolve) {
+  return new Promise(function (resolve) { // await一次
     console.log('promise1')
     resolve()
   })
+
+  /**
+   * 相当于
+   * var pro = await new Promise(function (resolve) {
+      console.log('promise1')
+      resolve()
+    })
+     return pro
+   */
 }
 
-async3().then(function (value) {
+async3().then(function (value) { // await两次, 因此排在'async1 end'后面
   console.log('async3.then:', value)
 })
 
