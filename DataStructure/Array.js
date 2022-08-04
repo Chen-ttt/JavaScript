45eea2114cb6a4de37782ac781d318fb12a23068
Tong Chen <1141425427@qq.com>
2022-08-04 17:14:30 +0100

    Usage of sort() API

--- DataStructure/Array.js
+++ DataStructure/Array.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: Tong Chen
  * @Date: 2022-07-13 19:30:09
- * @LastEditTime: 2022-07-27 23:22:38
+ * @LastEditTime: 2022-08-04 17:14:06
  * @LastEditors:  
  */
 /**
@@ -72,3 +72,38 @@ console.log("After fill:", arr1)
  */
 var arr2D = new Array(3).fill(0).map(() => new Array(4).fill(0))
 console.log("2D array:", arr2D)
+
+
+/**
+ * sort() - 将原地改动函数
+ * 对函数进行排序, 默认是按照字母升序(依照ASCII码)
+ * 
+ * 如果需要排序数字, 需要在参数中传入一个函数来指定如何排序
+ * sort(function(a, b){ // a在b后面
+ *  return true/正值 将交换a和b的位置
+ *  return false/负值 将不交换
+ * })
+ * 
+ * 在V8引擎中, 如果数组元素少于10个, 则sort使用的是插入排序;
+ * 多于10个, 则使用的是快速排序
+ */
+
+console.log(['a', 'c', 'b'].sort())
+// 但如果是数字比较, 会先调用toString() 将数字转为字符串比较
+console.log([45, 5, 412].sort())
+// 45会在5前面, 因为比较第一位4和5, 已经小于, 就不再向后比较
+// 412会在45前面, 因为第一位4相等, 比较第二位1和5, 1更小, 不在向后比较
+
+// 比较数字
+
+// 升序
+console.log([45, 5, 412].sort((a, b) => {
+  console.log(a, b) // a在b后面
+  return a - b // 返回负值, 说明a小于b(i.e.较后的那个元素更小), 交换; 正值, 不交换
+}))
+
+// 降序
+console.log([45, 5, 412].sort((a, b) => {
+  console.log(a, b)
+  return b - a // 反之
+}))
